c6579b8cfb292fa798100309e1c5745bd1dfb94f
Joao Pedro <dev.joaopedrovieira@gmail.com>
2021-11-04 16:42:46 +0000

    componentizaçao

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,6 +4,7 @@ import '../styles/sidebar.scss';
 
 import { api } from '../services/api';
 import { Button } from "./Button";
+import { MovieCard } from "./MovieCard";
 
 interface GenreResponseProps {
   id: number;
@@ -68,6 +69,21 @@ export function SideBar() {
         </div>
 
       </nav>
+
+      <div className="container">
+  <header>
+    <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+  </header>
+
+  <main>
+  <div className="movies-list">
+    {movies.map(movie => (
+      <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+    ))}
+  </div>
+  
+</main>
+</div>
 
 
     </div>
